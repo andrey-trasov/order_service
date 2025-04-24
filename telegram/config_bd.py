82ebf68689e77700b0d7a17494b4adde997a77a5
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2025-04-24 22:39:59 +0300

    редактирование docker

--- telegram/config_bd.py
+++ telegram/config_bd.py
@@ -1,9 +1,10 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 
 # 1. Определите параметры подключения к базе данных
-DATABASE_URL = "postgresql://user:password@host:port/database_name"  # Замените на ваши значения
-
+DATABASE_URL = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
 # 2. Создайте движок SQLAlchemy
 engine = create_engine(DATABASE_URL)
 
@@ -13,4 +14,4 @@ Base = declarative_base()
 # Создание фабрики сессий
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
+# DATABASE_URL = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
